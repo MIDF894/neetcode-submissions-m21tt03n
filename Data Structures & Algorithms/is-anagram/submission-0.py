16bd1f9429289df2852935c6bff92b4088e492c5
MIDF894 <umerumersij@gmail.com>
2026-05-06 23:10:50 +0500

    Delete submission-0.py

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        def toArr(stirng: str):
-            result = []
-            for i in stirng:
-                result.append(i)
-            return result
-        return sorted(toArr(s)) == sorted(toArr(t))
